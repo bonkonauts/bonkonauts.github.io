75d85ef5cd72cea736db5bed68d903f8671ac899
IAmNotHacking <71348579+IAmNotHacking@users.noreply.github.com>
2021-11-24 15:16:38 -0600

    Reformat skin page

--- avatars/index.js
+++ avatars/index.js
@@ -2,7 +2,7 @@ function init() {
 	const AVATARS = [window.user.avatar1, window.user.avatar2, window.user.avatar3, window.user.avatar4, window.user.avatar5];
 	const ACTIVE_AVATAR = window.user.activeAvatarNumber;
 
-	var tmpCard, tmpAvatar, tmpActive, tmpNote;
+	var tmpCard, tmpAvatar, tmpActive, tmpPreview, tmpBL;
 	tmpActive = document.createElement('active');
 
 	var avatarContainer = document.querySelector('section.content');
@@ -12,16 +12,27 @@ function init() {
 		let shareURL   = createShareURL(count, window.user.username, avatar); 
 		tmpCard = document.createElement('card');
 			tmpAvatar = document.createElement('avatar');
-				tmpAvatar.setAttribute('style', `background:url(${previewURL},#111);background-size: 100%;`)
+				tmpAvatar.setAttribute('style', `background:url(${previewURL},#111);background-size: 100%;`);
+				tmpAvatar.addEventListener('click', (e) => {
+					alert("This is not ready yet!")
+				});
 			tmpCard.appendChild(tmpAvatar);
+			tmpPreview = document.createElement('p');
+		        tmpPreview.className = "preview";
+			    tmpPreview.innerText = "Preview";
+			tmpCard.appendChild(tmpPreview);
+
 			if(count == ACTIVE_AVATAR) tmpCard.appendChild(tmpActive);
-			tmpNote = document.createElement('note');
-			tmpCard.appendChild(tmpNote);
+			tmpBL = document.createElement('div');
+                tmpBL.className = "bl";
+                tmpBL.innerHTML = "<p>open in BL</p><img src='https://bonkleagues.io/static/img/editor-logo.png'>";
+                tmpBL.addEventListener('click', (e) => {
+					window.open(shareURL, "_blank");
+				});
+			tmpCard.appendChild(tmpBL);
 		avatarContainer.appendChild(tmpCard);
 
-		tmpCard.addEventListener('click', (e) => {
-			window.open(shareURL, "_blank");
-		});
+		
 		count++;
 	}
 }
